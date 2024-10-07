81acf113cd5995793911cb99b9038bfd60667dac
Eloyo Poyo Ceboyo <eloyagudoc@gmail.com>
2024-10-07 20:40:45 +0200

    first commit

--- redsocial.php
+++ redsocial.php
@@ -217,7 +217,7 @@
             </ul>
         </nav>
 
-        
+        <!--AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA -->
         <!-- Parte central con artículos -->
         <main>
             <!-- Primer artículo -->
